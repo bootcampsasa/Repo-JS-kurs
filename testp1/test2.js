a32c2126e68c21ce9bf3d97d1844fe9596be26b2
Aleksandar Raskov <araskov@gmail.com>
2019-11-07 11:32:31 +0100

    test popravni

---
+++ testp1/test2.js
@@ -0,0 +1,48 @@
+{
+    let n = 5;
+    let k = 3;
+    let m = 2;
+
+    for (i = 1; i <= n; i++) {
+
+        if (i % m == 0)
+            console.log(`${i}. JavaScript je najbolji programski jezik`);
+        if (i % k == 0) {
+            console.log(`${i}. JavaScript je strava`);
+            console.log(`${i}. Ja volim JavaScript`);
+        }
+        else
+            console.log(`${i}. Ja volim JavaScript`)
+    }
+
+    //25
+}
+
+
+{
+    let start = 25;
+    let distance = 15;
+    let unit = 30;
+    let budget = 480;
+    let price = distance * unit + start;
+  //20
+    if(budget>=price){
+    console.log('Ima para')}
+    else{
+        console.log('Nema para')
+    }
+    //5
+    
+    
+}
+
+
+let bk=10;
+let tz;
+switch(tz){
+    case 
+}
+
+
+
+
